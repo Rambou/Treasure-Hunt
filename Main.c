88d178eefed94f567374c9a9d7d7974bbf94fb1a
Rambou <icsd10124@icsd.aegean.gr>
2015-12-08 03:34:57 +0200

    Additions, Fixes, bugs etc
    Placed some additions,  found and fixed some bugs in the game. Simplify
    and beautify code.

--- Main.c
+++ Main.c
@@ -13,157 +13,203 @@
 #if defined(_WIN32) || defined(WIN32) || defined(__CYGWIN__) || defined(__MINGW32__) || defined(__BORLANDC__)
 #define OS_WIN
 #endif
-int main ()
-{
-    // Basically, we reset the console to unbuffered
-    #ifdef  OS_WIN
-    DWORD        mode;
-    HANDLE       hstdin;
-    hstdin = GetStdHandle( STD_INPUT_HANDLE );
-    GetConsoleMode( hstdin, &mode );
-    SetConsoleMode( hstdin, 0 );
-    #else
 
-    #endif // OS_WIN
+int main () {
+    // Show menu with game info
+    printf("*****************************************");
+    printf("\n*\t\tMade by Rambou\t\t*");
+    printf("\n*\tGoal of the game if to find\t*");
+    printf("\n*\tall the hidden treasures in\t*");
+    printf("\n*\tthe game.\t\t\t*");
+    printf("\n*\t\t\t\t\t*");
+    printf("\n*\tMove by using:\t\t\t*");
+    printf("\n*\t\t\t\t\t*");
+    printf("\n*\t  w\t\t8\t\t*");
+    printf("\n*\ta   d\tor   4     6\t\t*");
+    printf("\n*\t  d\t\t2\t\t*");
+    printf("\n*\t\t\t\t\t*");
+    printf("\n*****************************************");
+    printf("\n\nPress S, to start game.\n");
+    setConsoleBuffered(0);
+    char result;
+    while(result!='s'&& result!='S') {
+        result = getchar();
+    }
+    setConsoleBuffered(1);
+    system("cls");
 
     int arr[R][C];
 
     /* random number generator */
     srand (time(NULL));
 
-    // Δημιουργία κουτιού
-    for (int i=0;i<R;i++)
-        for (int j=0;j<C;j++)
-            if(i==0 || i==R-1 || j==0 || j==C-1){
+    /* Create game tablet
+     * obstacles to the borders.
+     */
+    for (int i=0; i<R; i++)
+        for (int j=0; j<C; j++)
+            if(i==0 || i==R-1 || j==0 || j==C-1) {
                 arr[i][j]=OBSTACLE;
-            }else{
+            } else {
                 arr[i][j]=SPACE;
             }
-    // Βάλε τον παίκτη σε μια τυχαία θέση
+    /* Place player in a random position
+     * in the range of [1,(R-2)]. Player
+     * should be inside borders.
+     */
     int pi=rand() % (R-2) + 1, pj=rand() % (C-2) + 1;
     arr[pi][pj]==3;
 
-    // δημιουργία εμπορδίων
-    for (int i=0;i<OBSTACLES;i++){
-        int k=rand() % (R-2) + 1, l=rand() % (C-2) + 1;
-        int flag = 1;
-        while(flag){
-            flag=0;
-
-            // έλεγχος αν η θέση ειναι πάικτης
-            if(arr[k][l]==3){
-                flag=1;
-                continue;
-            }
-            // γίνεται έλεγχος για γειτονικά εμπόδια
-            for(int a=k-1;a<=k+1;a++)
-                for(int s=l-1;s<=l+1;s++){
-                    //printf("k=%i-%i l=%i-%i arr=%i\n",k,a,l,s,arr[a][s]);
-                    if(arr[a][s]==1){
-                        flag = 1;
-                        break;
-                    }
-                }
-
-            // εάν υπάρχει έστω και ένα κοντινό δώσε καινούργιες τιμές στα K,L
-            if(flag){
-                k=rand() % (R-2) + 1;
-                l=rand() % (C-2) + 1;
-            }
-        }
-        // θέσε το τυχαίο εμππόδιο
-        arr[k][l]=1;
-    }
-
-    // δημιουργία θησαυρού
-    for (int i=0;i<TREASURES;i++){
-        int k=rand() % (R-2) + 1, l=rand() % (C-2) + 1;
-        while(arr[k][l]==1 || arr[k][l]==3)
-        {
+    // Create obstacles
+    for (int i=0; i<OBSTACLES; i++) {
+        /* Place obstacle in a random position
+         * in the range of [1,(R-2)] for X axis and
+         * [1,(C-2)] for Y axis. Obstacles should be
+         * inside borders.
+         */
+        int k, l;
+TryAgain:
+        while(1) {
             k=rand() % (R-2) + 1;
             l=rand() % (C-2) + 1;
+
+            // Check if coordinates is a player and try again
+            if(arr[k][l]==PLAYER ) {
+                continue;
+            }
+            // Check for neighbour obstacles
+            for(int a=k-1; a<=k+1; a++)
+                for(int s=l-1; s<=l+1; s++) {
+                    // If you found at least one then try again
+                    if(arr[a][s]==OBSTACLE) {
+                        goto TryAgain;
+                    }
+                }
+            break;
         }
-        arr[k][l]=2;
+        arr[k][l]=OBSTACLE; // Create obstacle
     }
 
-    // Ξεκινάει το παιχνίδι
-    char result = '1';
-    int treasure=TREASURES;
-    while(result!='0' && treasure>0){
-        int Vflag = 0;
-        int Hflag = 0;
+    // Create treasures
+    for (int i=0; i<TREASURES; i++) {
+        int k,l;
+        do {
+            // Random coordinates between borders
+            k=rand() % (R-2) + 1;
+            l=rand() % (C-2) + 1;
+        } while(arr[k][l]==OBSTACLE || arr[k][l]==PLAYER || arr[k][l]==HIDDEN_TREASURE);
+        arr[k][l]=2; // Create treasure
+    }
+
+    // Start game
+    result = '1';
+    int tleft=TREASURES;
+    while(result!='0' && tleft>0) {
+        int moved = 0;
         switch(result) {
-            case '8' :
-                if(arr[pi-1][pj]==SPACE || arr[pi-1][pj]==HIDDEN_TREASURE || arr[pi-1][pj]==PLAYER || arr[pi-1][pj]==FOUND_TREASURE){
-                    if(arr[pi][pj]==PLAYER) arr[pi][pj]=SPACE;
-                    pi--;
-                    Vflag=1;
-                }
-                break;
-            case '2' :
-                if(arr[pi+1][pj]==SPACE || arr[pi+1][pj]==HIDDEN_TREASURE || arr[pi+1][pj]==PLAYER || arr[pi+1][pj]==FOUND_TREASURE){
-                    if(arr[pi][pj]==PLAYER) arr[pi][pj]=SPACE;
-                    pi++;
-                    Hflag=1;
-                }
-                break;
-            case '4' :
-                if(arr[pi][pj-1]==SPACE || arr[pi][pj-1]==HIDDEN_TREASURE || arr[pi][pj-1]==PLAYER || arr[pi][pj-1]==FOUND_TREASURE){
-                    if(arr[pi][pj]==PLAYER) arr[pi][pj]=SPACE;
-                    pj--;
-                    Vflag=1;
-                }
-                break;
-            case '6':
-                if(arr[pi][pj+1]==SPACE || arr[pi][pj+1]==HIDDEN_TREASURE || arr[pi][pj+1]==PLAYER || arr[pi][pj+1]==FOUND_TREASURE){
-                    if(arr[pi][pj]==PLAYER) arr[pi][pj]=SPACE;
-                    pj++;
-                    Hflag=1;
-                }
-                break;
+        case '8':
+        case 'w':
+        case 'W':
+            if(arr[pi-1][pj]==SPACE || arr[pi-1][pj]==HIDDEN_TREASURE || arr[pi-1][pj]==PLAYER || arr[pi-1][pj]==FOUND_TREASURE) {
+                if(arr[pi][pj]==PLAYER) arr[pi][pj]=SPACE;
+                pi--;
+                moved=1;
+            }
+            break;
+        case '2':
+        case 's':
+        case 'S':
+            if(arr[pi+1][pj]==SPACE || arr[pi+1][pj]==HIDDEN_TREASURE || arr[pi+1][pj]==PLAYER || arr[pi+1][pj]==FOUND_TREASURE) {
+                if(arr[pi][pj]==PLAYER) arr[pi][pj]=SPACE;
+                pi++;
+                moved=1;
+            }
+            break;
+        case '4':
+        case 'a':
+        case 'A':
+            if(arr[pi][pj-1]==SPACE || arr[pi][pj-1]==HIDDEN_TREASURE || arr[pi][pj-1]==PLAYER || arr[pi][pj-1]==FOUND_TREASURE) {
+                if(arr[pi][pj]==PLAYER) arr[pi][pj]=SPACE;
+                pj--;
+                moved=1;
+            }
+            break;
+        case '6':
+        case 'd':
+        case 'D':
+            if(arr[pi][pj+1]==SPACE || arr[pi][pj+1]==HIDDEN_TREASURE || arr[pi][pj+1]==PLAYER || arr[pi][pj+1]==FOUND_TREASURE) {
+                if(arr[pi][pj]==PLAYER) arr[pi][pj]=SPACE;
+                pj++;
+                moved=1;
+            }
+            break;
         }
 
-        if(Vflag || Hflag){
-            if(arr[pi][pj]==HIDDEN_TREASURE){
+        if(moved) {
+            if(arr[pi][pj]==HIDDEN_TREASURE) {
                 arr[pi][pj]=4;
-                treasure--;
-            }else if(arr[pi][pj]!=FOUND_TREASURE){
+                printf("\a");
+                tleft--;
+            } else if(arr[pi][pj]!=FOUND_TREASURE) {
                 arr[pi][pj]=PLAYER;
             }
-            Vflag=0;
-            Hflag=0;
+            moved=0;
         }
 
-        // τύπωσε ταμπλό
-        for (int i=0;i<R;i++){
-            for (int j=0;j<C;j++){
-                if(arr[i][j]==OBSTACLE){
+        // Print game table
+        for (int i=0; i<R; i++) {
+            for (int j=0; j<C; j++) {
+                if(arr[i][j]==OBSTACLE) {
                     printf("X");
-                }else if(arr[i][j]==HIDDEN_TREASURE){
-                    printf(" ");
-                }else if(arr[i][j]==FOUND_TREASURE){
-                    printf("T");
-                }else if(arr[i][j]==PLAYER){
+                } else if(arr[i][j]==PLAYER) {
                     printf("O");
-                }else if(arr[i][j]==SPACE){
-                     printf(" ");
+                } else if(arr[i][j]==FOUND_TREASURE) {
+                    printf("T");
+                } else{
+                    printf(" ");
                 }
             }
             printf("\n");
         }
 
-        printf("Έχεις ακόμα %i θησαυρούς να βρεις!",treasure);
-        // Escape character to help clear the buffer
-        printf("\n");
-        if(treasure==0){
+        if(tleft==0) {
             system("cls");
-            printf("Συγχαρητήρια βρήκες όλους τους θησαυρούς!!!");
+            printf("Congratulations, You have found all treasures!!!");
             break;
+        } else {
+            printf("You still have %i treasures to find.",tleft);
         }
+
+        // set console to unbuffered to read
+        setConsoleBuffered(0);
+        result = getchar();
+
+        // set console to buffer to clear
+        setConsoleBuffered(1);
+        system("cls");
+    }
+
+    setConsoleBuffered(0);
+    printf("\n\nPress X, to exit.");
+    while(result!='x'&& result!='X') {
         result = getchar();
     }
-    #ifdef OS_WIN
-    // Reset to normal input
-    SetConsoleMode(hstdin, mode);
-    #endif // OS_WIN
+}
+
+// Basically, we reset the console to unbuffered
+void setConsoleBuffered(int boolean) {
+#ifdef  OS_WIN
+    DWORD        mode;
+    HANDLE       hstdin;
+    hstdin = GetStdHandle( STD_INPUT_HANDLE );
+    GetConsoleMode( hstdin, &mode );
+    SetConsoleMode( hstdin, 0 );
+
+    if(boolean)
+        // Reset to normal input
+        SetConsoleMode(hstdin, mode);
+    else
+        // Reset to unbuffered mode
+        SetConsoleMode( hstdin, 0 );
+#endif // OS_WIN
 }
